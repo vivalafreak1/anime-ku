67ca262ebe02666a9c8ceb64de608039ef5ff1b4
vivalafreak1 <arieftaufikrahman123@gmail.com>
2024-05-23 11:05:12 +0700

    Fix icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,12 +2,14 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import Favicon from "/public/favicon.ico";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Wibu Nation",
   description: "Tempatnya para wibu yang suka nonton anime dan manga",
+  icons: [{ rel: "icon", url: Favicon.src }],
 };
 
 export default function RootLayout({ children }) {
