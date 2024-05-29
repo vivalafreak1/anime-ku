af6a3b532fafdcbd6eb0669e6f12c599dc42b6d8
vivalafreak1 <arieftaufikrahman123@gmail.com>
2024-05-29 12:48:21 +0700

    Fix search, footer, delete manga section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,4 @@
 import AnimeList from "@/components/AnimeList";
-import MangaList from "@/components/MangaList";
 import Header from "@/components/AnimeList/Header";
 import {
   getAnimeResponse,
@@ -10,7 +9,6 @@ import {
 export default async function Page() {
   const topAnime = await getAnimeResponse("top/anime", "limit=10");
   const ongoingAnime = await getAnimeResponse("seasons/now", "limit=10");
-  const topManga = await getAnimeResponse("top/manga", "limit=10");
   return (
     <>
       {/* Anime Populer */}
@@ -32,17 +30,6 @@ export default async function Page() {
           linkHref="/ongoing"
         />
         <AnimeList api={ongoingAnime} />
-      </section>
-      {/* Divider */}
-      <hr className="w-full mx-auto mb-8 border-t border-color-secondary max-w-screen-2xl" />
-      {/* Manga Populer */}
-      <section>
-        <Header
-          title="Manga populer"
-          linkTitle="Lihat semua"
-          linkHref="/ongoing"
-        />
-        <MangaList api={topManga} />
       </section>
     </>
   );
--- src/app/search/page.jsx
+++ src/app/search/page.jsx
@@ -6,6 +6,87 @@ import { getAnimeResponse } from "@/libs/api";
 import AnimeList from "@/components/AnimeList";
 import Header from "@/components/AnimeList/Header";
 import ReactLoading from "react-loading";
+import Select from "react-select";
+
+// Define the genre options
+const genreOptions = [
+  { value: 1, label: "Action" },
+  { value: 2, label: "Adventure" },
+  { value: 3, label: "Racing" },
+  { value: 4, label: "Comedy" },
+  { value: 5, label: "Avant Garde" },
+  { value: 6, label: "Mythology" },
+  { value: 7, label: "Mystery" },
+  { value: 8, label: "Drama" },
+  { value: 9, label: "Ecchi" },
+  { value: 10, label: "Fantasy" },
+  { value: 11, label: "Strategy" },
+  { value: 12, label: "Hentai" },
+  { value: 13, label: "Historical" },
+  { value: 14, label: "Horror" },
+  { value: 15, label: "Kids" },
+  { value: 17, label: "Martial Arts" },
+  { value: 18, label: "Mecha" },
+  { value: 19, label: "Music" },
+  { value: 20, label: "Parody" },
+  { value: 21, label: "Samurai" },
+  { value: 22, label: "Romance" },
+  { value: 23, label: "School" },
+  { value: 24, label: "Sci-Fi" },
+  { value: 25, label: "Shoujo" },
+  { value: 26, label: "Girls Love" },
+  { value: 27, label: "Shounen" },
+  { value: 28, label: "Boys Love" },
+  { value: 29, label: "Space" },
+  { value: 30, label: "Sports" },
+  { value: 31, label: "Super Power" },
+  { value: 32, label: "Vampire" },
+  { value: 35, label: "Harem" },
+  { value: 36, label: "Slice of Life" },
+  { value: 37, label: "Supernatural" },
+  { value: 38, label: "Military" },
+  { value: 39, label: "Detective" },
+  { value: 40, label: "Psychological" },
+  { value: 41, label: "Suspense" },
+  { value: 42, label: "Seinen" },
+  { value: 43, label: "Josei" },
+  { value: 46, label: "Award Winning" },
+  { value: 47, label: "Gourmet" },
+  { value: 48, label: "Workplace" },
+  { value: 49, label: "Erotica" },
+  { value: 50, label: "Adult Cast" },
+  { value: 51, label: "Anthropomorphic" },
+  { value: 52, label: "CGDCT" },
+  { value: 53, label: "Childcare" },
+  { value: 54, label: "Combat Sports" },
+  { value: 55, label: "Delinquents" },
+  { value: 56, label: "Educational" },
+  { value: 57, label: "Gag Humor" },
+  { value: 58, label: "Gore" },
+  { value: 59, label: "High Stakes Game" },
+  { value: 60, label: "Idols (Female)" },
+  { value: 61, label: "Idols (Male)" },
+  { value: 62, label: "Isekai" },
+  { value: 63, label: "Iyashikei" },
+  { value: 64, label: "Love Polygon" },
+  { value: 65, label: "Magical Sex Shift" },
+  { value: 66, label: "Mahou Shoujo" },
+  { value: 67, label: "Medical" },
+  { value: 68, label: "Organized Crime" },
+  { value: 69, label: "Otaku Culture" },
+  { value: 70, label: "Performing Arts" },
+  { value: 71, label: "Pets" },
+  { value: 72, label: "Reincarnation" },
+  { value: 73, label: "Reverse Harem" },
+  { value: 74, label: "Romantic Subtext" },
+  { value: 75, label: "Showbiz" },
+  { value: 76, label: "Survival" },
+  { value: 77, label: "Team Sports" },
+  { value: 78, label: "Time Travel" },
+  { value: 79, label: "Video Game" },
+  { value: 80, label: "Visual Arts" },
+  { value: 81, label: "Crossdressing" },
+];
 
 export default function SearchPage() {
   const router = useRouter();
@@ -22,6 +103,16 @@ export default function SearchPage() {
   const [searchAnime, setSearchAnime] = useState(null);
   // State for managing loading indicator
   const [isLoading, setIsLoading] = useState(true);
+  // State for selected genres
+  const [selectedGenres, setSelectedGenres] = useState(
+    genre
+      ? genre
+          .split(",")
+          .map((id) =>
+            genreOptions.find((option) => option.value === parseInt(id))
+          )
+      : []
+  );
 
   useEffect(() => {
     async function fetchAnime() {
@@ -51,7 +142,7 @@ export default function SearchPage() {
     const keyword = form.keyword.value.trim();
     const startYear = form.startYear.value.trim();
     const endYear = form.endYear.value.trim();
-    const genre = form.genre.value.trim();
+    const genre = selectedGenres.map((option) => option.value).join(",");
     const minScore = form.minScore.value.trim();
     const status = form.status.value.trim();
     const rating = form.rating.value.trim();
@@ -80,7 +171,7 @@ export default function SearchPage() {
           type="text"
           placeholder="Cari judul..."
           defaultValue={keyword}
-          className="w-full p-2 border rounded border-color-orange sm:w-1/2"
+          className="w-full p-2 border rounded border-color-orange sm:w-1/4"
         />
         <input
           name="startYear"
@@ -96,12 +187,13 @@ export default function SearchPage() {
           defaultValue={endYear}
           className="w-full p-2 border border-gray-300 rounded sm:w-1/6"
         />
-        <input
+        <Select
           name="genre"
-          type="text"
-          placeholder="Genre (comma-separated)"
-          defaultValue={genre}
-          className="w-full p-2 border border-gray-300 rounded sm:w-1/4"
+          isMulti
+          options={genreOptions}
+          value={selectedGenres}
+          onChange={setSelectedGenres}
+          className="w-full sm:w-1/2"
         />
         <input
           name="minScore"
